990d4149a9b0477929c174480ea6a2fee2cdd222
EsraAbdirashid <esraabdirashid294@gmail.com>
2026-08-10 10:04:38 +0300

    Done

--- script.js
+++ script.js
@@ -47,4 +47,126 @@ navItems.forEach((item) => {
 
 // ================= ADD TO CART =================
 
+let cartCount = 0;
 
+const cartCountElement = document.querySelector(".cart-count");
+const addToCartButtons = document.querySelectorAll(".add-to-cart");
+
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total strong");
+
+let cartProducts = [];
+
+addToCartButtons.forEach((button) => {
+
+    button.addEventListener("click", () => {
+
+        const productCard = button.closest(".product-card");
+
+        const productName =
+            productCard.querySelector(".product-info h3").textContent;
+
+        const productPrice =
+            productCard.querySelector(".product-bottom strong").textContent;
+
+        const productImage =
+            productCard.querySelector(".product-image img").src;
+
+        const price = parseFloat(
+            productPrice.replace("$", "").replace(",", "")
+        );
+
+        cartProducts.push({
+            name: productName,
+            price: price,
+            image: productImage
+        });
+
+        cartCount++;
+
+        cartCountElement.textContent = cartCount;
+
+        updateCart();
+
+        button.style.transform = "scale(1.15)";
+
+        setTimeout(() => {
+            button.style.transform = "scale(1)";
+        }, 200);
+
+    });
+
+});
+
+
+function updateCart() {
+
+    cartItems.innerHTML = "";
+
+    let total = 0;
+
+    cartProducts.forEach((product, index) => {
+
+        total += product.price;
+
+        const item = document.createElement("div");
+
+        item.classList.add("cart-item");
+
+        item.innerHTML = `
+            <img src="${product.image}" alt="${product.name}">
+
+            <div class="cart-item-info">
+
+                <h3>${product.name}</h3>
+
+                <strong>$${product.price.toFixed(2)}</strong>
+
+                <button class="remove-cart-item" data-index="${index}">
+                    <i class="fa-solid fa-trash"></i>
+                </button>
+
+            </div>
+        `;
+
+        cartItems.appendChild(item);
+
+    });
+
+    cartTotal.textContent = `$${total.toFixed(2)}`;
+
+    if (cartProducts.length === 0) {
+
+        cartItems.innerHTML = `
+            <p class="empty-cart">
+                Your cart is empty.
+            </p>
+        `;
+
+    }
+
+
+    // REMOVE PRODUCT
+
+    const removeButtons =
+        document.querySelectorAll(".remove-cart-item");
+
+    removeButtons.forEach((button) => {
+
+        button.addEventListener("click", () => {
+
+            const index = button.dataset.index;
+
+            cartProducts.splice(index, 1);
+
+            cartCount--;
+
+            cartCountElement.textContent = cartCount;
+
+            updateCart();
+
+        });
+
+    });
+
+}
