11ec73a12eef8f1e61a7afbbb2782a39b1f3cf8b
EsraAbdirashid <esraabdirashid294@gmail.com>
2026-08-10 09:33:29 +0300

    Done

--- script.js
+++ script.js
@@ -43,3 +43,48 @@ navItems.forEach((item) => {
     });
 
 });
+
+
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+const menuIcon = menuToggle.querySelector("i");
+
+menuToggle.addEventListener("click", () => {
+
+    navLinks.classList.toggle("active");
+    menuToggle.classList.toggle("active");
+
+    if (navLinks.classList.contains("active")) {
+
+        menuIcon.classList.remove("fa-bars");
+        menuIcon.classList.add("fa-xmark");
+
+        menuToggle.setAttribute("aria-label", "Close menu");
+
+    } else {
+
+        menuIcon.classList.remove("fa-xmark");
+        menuIcon.classList.add("fa-bars");
+
+        menuToggle.setAttribute("aria-label", "Open menu");
+    }
+
+});
+
+const navItems = document.querySelectorAll(".nav-links a");
+
+navItems.forEach((item) => {
+
+    item.addEventListener("click", () => {
+
+        navLinks.classList.remove("active");
+        menuToggle.classList.remove("active");
+
+        menuIcon.classList.remove("fa-xmark");
+        menuIcon.classList.add("fa-bars");
+
+        menuToggle.setAttribute("aria-label", "Open menu");
+
+    });
+
+});
